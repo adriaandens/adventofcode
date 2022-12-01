5a5e19b61d5d1d73a19a7c1cdb5a7bd834870bf8
Adriaan <ballekes@ballekes.be>
2022-12-01 21:08:22 +0100

    Adding a bit of comments

--- 2022/01/x.pl
+++ 2022/01/x.pl
@@ -1,3 +1,6 @@
-map { $i++ if ! /\d+/; $_[$i] += $_ } <>;
-my @a = sort { $a <=> $b } @_;
-print $a[-1];
+my @a = sort { $b <=> $a } map { $i++ if /^$/; $_[$i] += $_ } <>;
+print $a[0];
+
+__END__
+The map returns the value of the last statement, which is what's in $_[$i].
+It doesn't matter that we also output the intermediate values of summing since the final one will always be bigger (no negative input).
--- 2022/01/y.pl
+++ 2022/01/y.pl
@@ -1,3 +1,8 @@
-map { $i++ if ! /\d+/; $_[$i] += $_ } <>;
-my @a = sort { $a <=> $b } @_;
-print $a[-1] + $a[-2] + $a[-3];
+map { $i++ if /^$/; $_[$i] += $_ } <>;
+my @a = sort { $b <=> $a } @_;
+print $a[0]+$a[1]+$a[2];
+
+__END__
+Inlining the sort and the map as in x.pl is wrong because map will return all elements, also the intermediate states of calory counting.
+If one Elf has crazy amounts of calories, his intermediate values will rank in the Top 3, giving a wrong result.
+That's why we need to be break and the final values, stored in @_ after the map.
