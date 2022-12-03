ee0973926065aa339e0651ad69cbf91f2311af91
Adriaan <ballekes@ballekes.be>
2022-12-03 11:07:25 +0100

    Compressing...

--- 2022/03/x.pl
+++ 2022/03/x.pl
@@ -1,34 +1,17 @@
-my $total;
 map {
-	my %items = ();
-	chomp;
-	my $length = y///c;
-	print "String has length: $length\n";
-	my $i = 0;
-	foreach(split //) { 
-		if($i < $length / 2) {
-			print "$_ belongs to the first part\n";
-			$items{$_} = 0;
-		} else {
-			print "$_ belongs to the second part\n";
-			if(defined($items{$_})) {
-				print "$_ is the double item.\n";
-				if(ord($_) >= ord('a')) {
-					my $priority = ord($_) - ord('a') + 1;
-					print "Priority is $priority\n";
-					$total += $priority;
-				} else {
-					my $priority = ord($_) - ord('A') + 27;
-					print "Priority is $priority\n";
-					$total += $priority;
-				}
-				last;
-			}
-		}
+	%b = (); $i=0; chomp; $l = y///c; # Initializing some vars
+	foreach(/./g) { 
+		$b{$_} = 0 if $i < $l / 2;
+		$t += ord($_) >= ord('a') ? ord($_) - ord('a') + 1 : ord($_) - ord('A') + 27 if defined($b{$_}) && $i >= $l / 2;
+		last if defined($b{$_}) && $i >= $l / 2;
 		$i++;
 	}
 } <>;
-print "Solution: $total\n";
+print $t;
 
 __END__
 Needed to use a foreach for the inner loop because map {} does not support the "last" keyword. And there were cases were the same letter would appear more than once in the second part, causing double counting of priorities.
+
+Golf techniques:
+* y///c is the same as length($_)
+* /./g is the same of split //
--- 2022/03/y.pl
+++ 2022/03/y.pl
@@ -1,39 +1,10 @@
-my $total;
-my %items = ();
 while(<>) {
-	chomp;
-	print "Line $.\n";
-
-	my $a = $. % 3;
-	print "a is $a\n";
-
-	my %line = ();
-	map { $line{$_} = 1 } split //;
-	map { $items{$_} += $a + 1 } keys(%line);
-
-	if($a == 0) {
-		print "Final Elf of a group.\n";
-
-		map { 
-			
-			if($items{$_} == 6) {
-				print "This char appears in all three: $_\n";
-				if(ord($_) >= ord('a')) {
-					my $priority = ord($_) - ord('a') + 1;
-					print "Priority is $priority\n";
-					$total += $priority;
-				} else {
-					my $priority = ord($_) - ord('A') + 27;
-					print "Priority is $priority\n";
-					$total += $priority;
-				}
-			}
-		} keys(%items);
-		
-		# CLEANUP
-		%items = ();
-	}
+	chomp; $a = $. % 3; %l = ();
+	map { $l{$_} = 1 } /./g;
+	map { $i{$_} += $a + 1 } keys(%l);
+	map { $t += ord($_) >= ord('a') ? ord($_) - ord('a') + 1 : ord($_) - ord('A') + 27 if $i{$_} == 6 } keys(%i) if $a == 0;
+	%i = () if $a == 0;
 }
-print "Solution: $total\n";
+print $t;
 
 
