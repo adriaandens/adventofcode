1581ef7b4a3625ea33f2dbfc79c3720f0c3df59b
Adriaan <ballekes@ballekes.be>
2022-12-04 09:31:07 +0100

    Turning off verbose mode

--- 2022/04/x.pl
+++ 2022/04/x.pl
@@ -1,15 +1,5 @@
-my $total;
 map {
-	my ($a_start, $a_end, $b_start, $b_end) = /(\d+)-(\d+),(\d+)-(\d+)/;
-	print "A gaat van $a_start tot $a_end\n";
-	print "B gaat van $b_start tot $b_end\n";
-
-	if($a_start <= $b_start && $a_end >= $b_end) {
-		print "Elf A contains fully what B will do.\n";
-		$total++;
-	} elsif($b_start <= $a_start && $b_end >= $a_end) {
-		print "Elf B contains fully what A will do.\n";
-		$total++;
-	}
+	/(\d+)-(\d+),(\d+)-(\d+)/;
+	$total++ if ($1 <= $3 && $2 >= $4) || ($3 <= $1 && $4 >= $2);
 } <>;
-print "Solution: $total\n";
+print $total;
--- 2022/04/y.pl
+++ 2022/04/y.pl
@@ -1,11 +1,5 @@
-my $total;
 map {
-	my ($a_start, $a_end, $b_start, $b_end) = /(\d+)-(\d+),(\d+)-(\d+)/;
-	print "A gaat van $a_start tot $a_end\n";
-	print "B gaat van $b_start tot $b_end\n";
-
-	if(! ($a_end < $b_start || $b_end < $a_start)) {
-		$total++;
-	}
+	/(\d+)-(\d+),(\d+)-(\d+)/;
+	$total++ if ! ($2 < $3 || $4 < $1);
 } <>;
-print "Solution: $total\n";
+print $total;
