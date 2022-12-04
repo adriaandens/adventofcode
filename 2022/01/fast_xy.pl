8a44d4646ab0d12f22dc24e0d1222e05a491cc86
Adriaan <ballekes@ballekes.be>
2022-12-04 16:35:27 +0100

    Open()ing the file in Perl (12s)

--- 2022/01/fast_xy.pl
+++ 2022/01/fast_xy.pl
@@ -1,4 +1,5 @@
-while(<>) {
+open(F, '<', '/home/adri/Unsorted/aoc_2022_day01_large_input.txt') or die 'Cannot find file';
+while(<F>) {
 	if(length == 1) {
 		if($local_sum >= $max) {
 			$max3 = $max2;
@@ -15,13 +16,13 @@ while(<>) {
 		$local_sum += $_;
 	}
 }
-print $max . "\n";
-print $max + $max2 + $max3 . "\n";
+print $max . $/;
+print $max + $max2 + $max3 . $/;
 
 __END__
 Background: https://gathering.tweakers.net/forum/list_message/73652172#73652172 provides a 493MB input file to test our implementations on.
 
-In the initial implementation `sort {}` is used, which is at best O(nlogn) if my memory of algorithms is correct, so that would make it way too slow. So we change our implementation to just keep track of the top 3 values instead.
+In the initial implementation `sort {}` is used, which is at best O(nlogn) if my memory of algorithms is correct, so that would make it way too slow. So we change our implementation to just keep track of the top 3 values instead (O(n)).
 
 First implementation of the O(n) scan:
 $ time perl fast_xy.pl < ~/Unsorted/aoc_2022_day01_large_input.txt
@@ -30,7 +31,7 @@ $ time perl fast_xy.pl < ~/Unsorted/aoc_2022_day01_large_input.txt
 
 real	0m39.708s
 user	0m35.174s
-sys		0m4.528s
+sys	0m4.528s
 
 Changing the regex with a dumb length check (the empty line '\n' has length 1):
 $ time perl fast_xy.pl < ~/Unsorted/aoc_2022_day01_large_input.txt 
@@ -39,7 +40,7 @@ $ time perl fast_xy.pl < ~/Unsorted/aoc_2022_day01_large_input.txt
 
 real	0m34.893s
 user	0m29.955s
-sys		0m4.936s
+sys	0m4.936s
 -> Saves us 5 seconds
 
 Changing the `map {} <>` to a `while(<>) {}` gives also a signifcant speed up:
@@ -49,5 +50,17 @@ $ time perl fast_xy.pl < ~/Unsorted/aoc_2022_day01_large_input.txt
 
 real	0m12.601s
 user	0m12.537s
-sys		0m0.056s
+sys	0m0.056s
 -> Shaves of an impressive 22 seconds
+
+Taking over IO and `open()`ing the file inside Perl instead of passing it via the shell.
+$ time perl fast_xy.pl
+184028272
+549010145
+
+real	0m12.423s
+user	0m12.366s
+sys	0m0.056s
+-> No real significant change.
+
+
