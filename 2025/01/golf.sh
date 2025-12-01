f7777816bf73581dade988e6e61e788b3e4fd1e4
Adriaan Dens <adriaan.dens@gmail.com>
2025-12-01 20:56:39 +0000

    bit of golfing

--- 2025/01/golf.sh
+++ 2025/01/golf.sh
@@ -1,2 +1,6 @@
+# deel 1
 perl -nle '/(\w)(\d+)/;$s=($s+($1eq"L"?-1*$2:$2))%100;$c++if$s==0}BEGIN{$s=50}END{print$c' < input.txt
 perl -nle '/(\w)(\d+)/;$s=($s+($1eq"L"?-1*$2:$2))%100;$c++if$s==0}BEGIN{$s=50}END{print$c' < aoc-2025-day-1-challenge-1.txt
+# deel 2
+perl -nle '/(\w)(\d+)/;$a=$1eq"L"?-1*$2:$2;my$mod=1if$1eq"L"&&$s>0;$c+=int(abs($s+$a)/100)+$mod if$s+$a<=0||$s+$a>=100;$s=($s+$a)%100;}BEGIN{$s=50}END{print$c' < input.txt
+perl -nle '/(\w)(\d+)/;$a=$1eq"L"?-1*$2:$2;my$mod=1if$1eq"L"&&$s>0;$c+=int(abs($s+$a)/100)+$mod if$s+$a<=0||$s+$a>=100;$s=($s+$a)%100;}BEGIN{$s=50}END{print$c' < aoc-2025-day-1-challenge-1.txt
